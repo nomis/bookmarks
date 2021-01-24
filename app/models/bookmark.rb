710f9d4812e419105c07d4d543ba07fa55ac39cd
Simon Arlott <sa.me.uk>
2021-01-24 17:03:28 +0000

    Simplify bookmark tag updates

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -44,45 +44,37 @@ class Bookmark < ApplicationRecord
   def save_tags_string
     return unless @new_tags
 
-    removed_tags = []
+    # Delete removed tags
+    removed_tags = tags.reject { |tag| @new_tags.include?(tag.key) }.each do |tag|
+      tags.delete(tag)
+    end
 
-    tags.each do |old_tag|
-      if @new_tags.include?(old_tag.key)
-        # Update case for the names of existing tags
-        if old_tag.name != @new_tags[old_tag.key].name
-          old_tag.name = @new_tags[old_tag.key].name
-          old_tag.save!
-        end
-      else
-        # Delete removed tags
-        removed_tags << old_tag.id
-        tags.delete(old_tag)
-      end
+    # Update case for the names of existing tags
+    tags.reject { |tag| tag.name == @new_tags[tag.key].name }.each do |tag|
+      tag.name = @new_tags[tag.key].name
+      tag.save!
     end
 
     # Add new tags but don't update the case of the tag name
-    old_keys = tags.map(&:key)
-    (@new_tags.keys - old_keys).each do |new_key|
+    (@new_tags.keys - tags.map(&:key)).each do |key|
       # Concurrency issue: the same tag may be created in another thread
       # (there is no way to resolve this here because we'd need a second
       # independent transaction and that could leave unreferenced tags if an
       # error occurs)
-      new_tag = Tag.find_by(key: new_key) || @new_tags[new_key]
-      tags << new_tag
+      tags << (Tag.find_by(key: key) || @new_tags[key])
     end
 
     # Delete tags that now have no bookmarks
-    unless removed_tags.empty?
-      # Concurrency issue: the last two users of the tag may remove it in
-      # two separate threads but neither of them can identify this within
-      # their own transaction, leaving the tag unreferenced
-      #
-      # Potential concurrency issue: the tag may be reused in another thread
-      # (databases may handle conflicts with this DELETE in different ways
-      # so it could silently ignore newly referenced tags or raise an error
-      # on the foreign key constraint)
-      Tag.where.missing(:bookmarks).where(id: removed_tags).delete_all
-    end
+    #
+    # Concurrency issue: the last two users of the tag may remove it in
+    # two separate threads but neither of them can identify this within
+    # their own transaction, leaving the tag unreferenced
+    #
+    # Potential concurrency issue: the tag may be reused in another thread
+    # (databases may handle conflicts with this DELETE in different ways
+    # so it could silently ignore newly referenced tags or raise an error
+    # on the foreign key constraint)
+    Tag.where.missing(:bookmarks).where(id: removed_tags.map(&:id)).delete_all unless removed_tags.empty?
 
     @new_tags = nil
   end
