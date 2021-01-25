e2ac0600f99244c748748bd9bca7af18f5b5137d
Simon Arlott <sa.me.uk>
2021-01-25 22:37:24 +0000

    Quote style fixes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   root "bookmarks#index"
   resources :bookmarks, constraints: { format: ["html", "json"] }
   get '/tags/:tags', to: 'bookmarks#search', as: :search_by_tags, constraints: { format: ["html", "json"] }
+  get "/tags/:tags", to: "bookmarks#search", as: :search_by_tags, constraints: { format: ["html", "json"] }
 end
