5a8ed72b858e9bf1305c33a70765b80f00558a30
Simon Arlott <sa.me.uk>
2021-07-25 11:08:24 +0100

    Add example user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create([{ username: "example", email: "example@example.com", password: "example", password_confirmation: "example"}])
