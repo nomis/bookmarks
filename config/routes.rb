a209050193761f696e3941c1cb815bfe394afbad
Simon Arlott <sa.me.uk>
2021-07-25 11:03:26 +0100

    Disable sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   root "bookmarks#index"
   resources :bookmarks, constraints: { format: ["html", "json"] }
   get "/bookmarks/:id/delete", to: "bookmarks#delete", as: :delete_bookmark, constraints: { format: ["html"] }
