207a8a5443a9daf80a718f67b2a90919ad65affc
Simon Arlott <sa.me.uk>
2021-08-06 19:35:38 +0100

    Use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2021_08_06_143241) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookmark_tags", id: false, force: :cascade do |t|
-    t.integer "bookmark_id", null: false
-    t.integer "tag_id", null: false
+    t.bigint "bookmark_id", null: false
+    t.bigint "tag_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["bookmark_id"], name: "index_bookmark_tags_on_bookmark_id"
