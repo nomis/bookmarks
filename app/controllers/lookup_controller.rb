605620b2a095719c0ccc4a82611eb2885636be7f
Simon Arlott <sa.me.uk>
2021-08-09 19:14:10 +0100

    Use a valid JSON object in lookup error responses

--- app/controllers/lookup_controller.rb
+++ app/controllers/lookup_controller.rb
@@ -10,24 +10,28 @@ class LookupController < ApplicationController
   def url
     lookup = LookupURI.new(params[:uri], params[:user_agent])
 
-    render(status: :bad_gateway, json: lookup.error) and return unless lookup.ok?
+    error(:bad_gateway, lookup.error) and return unless lookup.ok?
 
     respond_to do |format|
       format.json { render json: lookup.data }
     end
   rescue Timeout::Error, HTTP::TimeoutError => e
-    render(status: :gateway_timeout, json: nice_error_message(e))
+    error(:gateway_timeout, format_message(e))
   rescue HTTP::Error, OpenSSL::SSL::SSLError, Zlib::Error => e
-    render(status: :service_unavailable, json: nice_error_message(e))
+    error(:service_unavailable, format_message(e))
   rescue URI::InvalidURIError,
       Addressable::URI::InvalidURIError,
       LookupURI::ProhibitedURIError => e
-    render(status: :bad_request, json: nice_error_message(e))
+    error(:bad_request, format_message(e))
   end
 
   protected
 
-  def nice_error_message(e)
+  def error(status, message)
+    render(status: status, json: { "error" => message })
+  end
+
+  def format_message(e)
     klass = e.class.to_s
     message = e.message.to_s
 
