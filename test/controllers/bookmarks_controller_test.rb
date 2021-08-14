5e7df5dfd2b0beb0759a212e6cc7171e98cf9d9e
Simon Arlott <sa.me.uk>
2021-08-14 12:25:07 +0100

    Don't display tags heading or the untagged search link when there are no tags

--- test/controllers/bookmarks_controller_test.rb
+++ test/controllers/bookmarks_controller_test.rb
@@ -49,4 +49,66 @@ class BookmarksControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to root_url
   end
+
+  test "no tags heading when there are no tags" do
+    Tag.destroy_all
+    assert Bookmark.all.count > 0
+    assert Tag.all.count == 0
+
+    get bookmarks_url
+    assert_response :success
+
+    assert_select "h2", text: %r{^Bookmarks\b}
+    assert_select "h2", text: %r{^Tags\b}, count: 0
+  end
+
+  test "no untagged link when there are no tags" do
+    Tag.destroy_all
+    assert Bookmark.all.count > 0
+    assert Tag.all.count == 0
+
+    get bookmarks_url
+    assert_response :success
+
+    assert_select "a[href=?]", search_untagged_path, count: 0
+  end
+
+  test "tags heading when there are tags" do
+    b = Bookmark.first
+    b.tags_string = Tag.first.name
+    b.save!
+
+    b = Bookmark.second
+    b.tags_string = ""
+    b.save!
+
+    assert Bookmark.all.count > 0
+    assert Tag.all.count > 0
+    assert Tag.with_count.length > 0
+
+    get bookmarks_url
+    assert_response :success
+
+    assert_select "h2", text: %r{^Bookmarks\b}
+    assert_select "h2", text: %r{^Tags\b}
+  end
+
+  test "untagged link when there are tags" do
+    b = Bookmark.first
+    b.tags_string = Tag.first.name
+    b.save!
+
+    b = Bookmark.second
+    b.tags_string = ""
+    b.save!
+
+    assert Bookmark.all.count > 0
+    assert Tag.all.count > 0
+    assert Tag.with_count.length > 0
+
+    get bookmarks_url
+    assert_response :success
+
+    assert_select "a[href=?]", search_untagged_path
+  end
 end
