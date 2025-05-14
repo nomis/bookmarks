c25cd2a5fe687d43576bf106cc468d0bd100b215
Simon Arlott <sa.me.uk>
2025-05-14 18:26:33 +0100

    Use ActiveRecord instead of SQL for migrations

--- db/migrate/20250503145347_add_visibility_to_bookmark.rb
+++ db/migrate/20250503145347_add_visibility_to_bookmark.rb
@@ -4,10 +4,7 @@ class AddVisibilityToBookmark < ActiveRecord::Migration[6.1]
       change.up do
         add_column :bookmarks, :visibility, :integer, null: false, default: 0
 
-        execute <<-SQL
-          UPDATE bookmarks
-          SET visibility = (CASE WHEN private = true THEN 1 ELSE 0 END)
-        SQL
+        Bookmark.where(private: true).update_all("visibility=1")
 
         remove_index :bookmarks, [:created_at, :id],
           name: "index_bookmarks_on_created_at_and_id_where_public"
@@ -47,10 +44,8 @@ class AddVisibilityToBookmark < ActiveRecord::Migration[6.1]
           where: "#{connection.quote_column_name(:private)} = #{connection.quote(false)}",
           name: "index_bookmarks_on_created_at_and_id_where_public"
 
-        execute <<-SQL
-          UPDATE bookmarks
-          SET private = (CASE WHEN visibility = 0 THEN false ELSE true END)
-        SQL
+        Bookmark.where(visibility: "public").update_all("private=false")
+        Bookmark.where.not(visibility: "public").update_all("private=true")
 
         remove_column :bookmarks, :visibility
       end
--- db/migrate/20250503163511_add_visibility_to_user.rb
+++ db/migrate/20250503163511_add_visibility_to_user.rb
@@ -1,5 +1,15 @@
 class AddVisibilityToUser < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :visibility, :integer, null: false, default: 0
+    reversible do |change|
+      change.up do
+        add_column :users, :visibility, :integer, null: false, default: 0
+
+        User.update_all("visibility=1")
+      end
+
+      change.down do
+        remove_column :users, :visibility
+      end
+    end
   end
 end
